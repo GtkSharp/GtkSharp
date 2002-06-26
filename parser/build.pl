304427af77d7e0cc89a72c3b38d7302a2e09747b
Mike Kestner <mkestner@gmail.com>
2002-06-26 00:54:05 +0000

    2002-06-25  Mike Kestner <mkestner@speakeasy.net>
    	* generator/gtkapi.xml : lots of fixes, plus GtkHTML!
    	* parser/Gtk.metadata : add a bunch of renames.
    	* parser/build.pl : Add the gtkhtml parse.
    	* parser/README : module list to parse
    
    svn path=/trunk/gtk-sharp/; revision=5459

--- parser/build.pl
+++ parser/build.pl
@@ -8,7 +8,8 @@ unlink ($file);
 	  "pango-1.0.2/pango" => "Pango:pango-1.0",
 	  "gtk+-2.0.3/gdk" => "Gdk:gdk-x11-2.0",
 	  "gtk+-2.0.3/gdk-pixbuf" => "Gdk:gdk_pixbuf-2.0",
-	  "gtk+-2.0.3/gtk" => "Gtk:gtk-x11-2.0");
+	  "gtk+-2.0.3/gtk" => "Gtk:gtk-x11-2.0",
+	  "gtkhtml" => "Gtk:gtkhtml-2");
 
 foreach $dir (keys %srcs) {
 	($ns, $lib) = split (/:/, $srcs{$dir});
