d5954e56b87e8b5631ca5416381167d45ef27780
Rodrigo Moya <rodrigo@mono-cvs.ximian.com>
2002-09-01 11:21:43 +0000

    2002-09-01  Rodrigo Moya <rodrigo@ximian.com>
    	* gda/Application.cs: new class for libgda initialization and event
    	loop management.
    
    	* gda/Makefile.in: added new file as a dependency.
    
    svn path=/trunk/gtk-sharp/; revision=7170

---
+++ gda/Application.cs
@@ -0,0 +1,64 @@
+//
+// Gda.Application.cs - libgda initialization and event loop
+//
+// Author: Rodrigo Moya <rodrigo@ximian.com>
+//
+// (c) 2002 Rodrigo Moya
+//
+
+using System;
+using System.Runtime.InteropServices;
+
+namespace Gda
+{
+	/// <summary>
+	///   GDA Application class
+	/// </summary>
+	///
+	/// <remarks>
+	///   Provides the initialization and event loop iteration related
+	///   methods for the GDA data access library.
+	/// </remarks>
+
+	public class Application
+	{
+		[DllImport("gda-2")]
+		static extern void gda_init (string app_id, string version, int nargs, string[] args);
+
+		public static void Init ()
+		{
+			gda_init ("Gda#", "0.4", 0, new string[0]);
+		}
+
+		public static void Init (string app_id, string version)
+		{
+			gda_init (app_id, version, 0, new string[0]);
+		}
+
+		public static void Init (string[] args)
+		{
+			gda_init ("Gda#", "0.4", args.Length, args);
+		}
+
+		public static void Init (string app_id, string version, string[] args)
+		{
+			gda_init (app_id, version, args.Length, args);
+		}
+		
+		[DllImport("gda-2")]
+		static extern void gda_main_run (IntPtr init_func, IntPtr user_data);
+
+		public static void Run ()
+		{
+			gda_main_run (IntPtr.Zero, IntPtr.Zero);
+		}
+
+		[DllImport("gda-2")]
+		static extern void gda_main_quit ();
+
+		public static void Quit ()
+		{
+			gda_main_quit ();
+		}
+	}
+}
