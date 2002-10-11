52f04d5a617f9df7e58f9653d52aaafd26b5c484
Martin Baulig <martin@novell.com>
2002-10-11 15:41:49 +0000

    2002-10-11  Martin Baulig  <martin@gnome.org>
    	* glib/FileUtils.cs: New file.  Wrapped g_file_get_contents() here.
    
    svn path=/trunk/gtk-sharp/; revision=8167

---
+++ glib/FileUtils.cs
@@ -0,0 +1,29 @@
+// GLib.FileUtils.cs - GFileUtils class implementation
+//
+// Author: Martin Baulig <martin@gnome.org>
+//
+// (c) 2002 Ximian, Inc
+
+namespace GLib {
+
+	using System;
+	using System.Text;
+	using System.Runtime.InteropServices;
+
+	public class FileUtils
+	{
+		[DllImport("glib-2.0")]
+		extern static bool g_file_get_contents (string filename, out IntPtr contents, out int length, out IntPtr error);
+
+		public static string GetFileContents (string filename)
+		{
+			int length;
+			IntPtr contents, error;
+
+			if (!g_file_get_contents (filename, out contents, out length, out error))
+				throw new GException (error);
+
+			return Marshal.PtrToStringAnsi (contents, length);
+		}
+	}
+}
