77090286fa37d52e6009a4a99be4aed357c0ea66
Miguel de Icaza <miguel@gnome.org>
2002-10-19 21:44:31 +0000

    Add missing file
    svn path=/trunk/gtk-sharp/; revision=8402

---
+++ glue/combo.c
@@ -0,0 +1,13 @@
+/* combo.c : Glue for accessing fields in the GtkCombo widget.
+ *
+ * Author: Pablo Baena (pbaena@uol.com.ar)
+ *
+ */
+
+#include <gtk/gtkcombo.h>
+
+GtkWidget* 
+gtksharp_combo_get_entry (GtkCombo* combo)
+{
+	return combo->entry;
+}
