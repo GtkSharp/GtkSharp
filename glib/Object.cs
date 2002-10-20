f95bf9dbbdf72e1776e2672903d2af73b41df649
Miguel de Icaza <miguel@gnome.org>
2002-10-20 07:16:22 +0000

    2002-10-20  Miguel de Icaza  <miguel@ximian.com>
    	* glib/Object.cs: Avoid recursive calls with the previous operator
    	!= and operator ==
    
    svn path=/trunk/gtk-sharp/; revision=8418

--- glib/Object.cs
+++ glib/Object.cs
@@ -218,12 +218,38 @@ namespace GLib {
 
 		public static bool operator == (Object a, Object b)
 		{
-			return a.Equals (b);
+			object oa = a;
+			object ob = b;
+
+			if (ob == null){
+				if (oa == null)
+					return true;
+				else
+					return false;
+			} else {
+				if (oa == null)
+					return false;
+				else
+					return a.Equals (b);
+			}
 		}
 
 		public static bool operator != (Object a, Object b)
 		{
-			return !a.Equals (b);
+			object oa = a;
+			object ob = b;
+
+			if (ob == null){
+				if (oa == null)
+					return false;
+				else
+					return true;
+			} else {
+				if (oa == null)
+					return true;
+				else
+					return !a.Equals (b);
+			}
 		}
 		
 		/// <summary>
