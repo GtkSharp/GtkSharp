5dcbe5b0af49bf25450e8450e82f270d0ac94a43
Daniel Morgan <monodanmorg@yahoo.com>
2002-11-17 21:17:12 +0000

    2002-11-17  Daniel Morgan <danmorg@sc.rr.com>
    	* pango/Scale.cs: added file containing constants
    	for text widgets needing Pango Scale Attribute
    
    svn path=/trunk/gtk-sharp/; revision=9034

---
+++ pango/Scale.cs
@@ -0,0 +1,22 @@
+//
+// Scale.cs
+//
+// Author: Daniel Morgan <danmorg@sc.rr.com>
+//
+// Copyright (C) 2002 Daniel Morgan
+//
+
+namespace Pango {
+	
+	public sealed class Scale {
+		public static readonly double PangoScale = 1024.0;
+
+		public static readonly double XX_Small = 0.5787037037037;
+		public static readonly double X_Small = 0.6444444444444;
+		public static readonly double Small = 0.8333333333333;
+		public static readonly double Medium = 1.0;
+		public static readonly double Large = 1.2;
+		public static readonly double X_Large = 1.4399999999999;
+		public static readonly double XX_Large = 1.728;
+	}
+}
