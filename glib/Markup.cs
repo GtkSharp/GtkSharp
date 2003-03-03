e547fa0e3f4f2ccd49d4d6f786835f0bdb151c43
Miguel de Icaza <miguel@gnome.org>
2003-03-03 01:42:26 +0000

    2003-03-02  Miguel de Icaza  <miguel@ximian.com>
    	* glib/Markup.cs: Add new file.
    
    svn path=/trunk/gtk-sharp/; revision=12114

---
+++ glib/Markup.cs
@@ -0,0 +1,27 @@
+//
+// Markup.cs: Wrapper for the Markup code in Glib
+//
+// Authors:
+//    Miguel de Icaza (miguel@ximian.com)
+//
+// (C) 2003 Ximian, Inc.
+//
+using System;
+using System.Runtime.InteropServices;
+
+namespace GLib {
+
+
+	public class Markup {
+		[DllImport("libglib-2.0-0.dll")]
+		static extern string g_markup_escape_text (string text, int len);
+		
+		static public string EscapeText (string s)
+		{
+			if (s == null)
+				return "";
+
+			return g_markup_escape_text (s, s.Length);
+		}
+	}
+}
