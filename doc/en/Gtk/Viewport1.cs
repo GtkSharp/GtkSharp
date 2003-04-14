1efb61cb1e4db4d18a1d6a242e5ff04fcae3c00b
Lee Mallabone <lee@mono-cvs.ximian.com>
2003-04-14 18:35:24 +0000

    Add a compilable working sample for 'Viewport'.
    svn path=/trunk/gtk-sharp/; revision=13614

---
+++ doc/en/Gtk/Viewport1.cs
@@ -0,0 +1,52 @@
+// ViewportApp.cs - Gtk.Viewport class Test implementation
+//
+// Author: Lee Mallabone <gnome@fonicmonkey.net>
+//
+// (c) 2003 Lee Mallabone
+
+namespace GtkSamples {
+
+	using Gtk;
+	using GtkSharp;
+	using System;
+	using System.Drawing;
+
+	public class ViewportApp  {
+
+		public static ScrolledWindow CreateViewport()
+		{
+			ScrolledWindow scroller = new ScrolledWindow();
+			Viewport viewer = new Viewport();
+			
+			Table widgets = new Table(1, 2, false);
+			
+			widgets.Attach(new Entry("This is example Entry 1"), 0, 1, 0, 1);
+			widgets.Attach(new Entry("This is example Entry 2"), 1, 2, 0, 1);
+			
+			// Place the widgets in a Viewport, and the Viewport in a ScrolledWindow
+			viewer.Add(widgets);
+			scroller.Add(viewer);
+			return scroller;
+		}
+		
+		public static int Main (string[] args)
+		{
+			Application.Init ();
+			Window win = new Window ("Viewport Tester");
+			win.DefaultSize = new Size (180, 50);
+			win.DeleteEvent += new DeleteEventHandler (Window_Delete);
+			ScrolledWindow scroller = CreateViewport();
+			win.Add (scroller);
+			win.ShowAll ();
+			Application.Run ();
+			return 0;
+		}
+
+		static void Window_Delete (object obj, DeleteEventArgs args)
+		{
+			Application.Quit ();
+			args.RetVal = true;
+		}
+
+	}
+}
