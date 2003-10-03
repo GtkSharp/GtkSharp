27a0c64488c4b39f3943d7ec3d1ca8e847dabf50
Mike Kestner <mkestner@gmail.com>
2003-10-03 22:12:50 +0000

    oops, add the damn file
    svn path=/trunk/gtk-sharp/; revision=18571

---
+++ generator/AliasGen.cs
@@ -0,0 +1,90 @@
+// GtkSharp.Generation.AliasGen.cs - The Alias type Generatable.
+//
+// Author: Mike Kestner <mkestner@speakeasy.net>
+//
+// (c) 2003 Mike Kestner
+
+namespace GtkSharp.Generation {
+
+	using System;
+
+	public class AliasGen : IGeneratable  {
+		
+		string type;
+		string ctype;
+
+		public AliasGen (string ctype, string type)
+		{
+			this.ctype = ctype;
+			this.type = type;
+		}
+		
+		public string CName {
+			get
+			{
+				return ctype;
+			}
+		}
+
+		public string Name {
+			get
+			{
+				return type;
+			}
+		}
+
+		public string QualifiedName {
+			get
+			{
+				return type;
+			}
+		}
+
+		public string MarshalType {
+			get
+			{
+				return type;
+			}
+		}
+		public virtual string MarshalReturnType {
+			get
+			{
+				return type;
+			}
+		}
+
+		public string CallByName (string var_name)
+		{
+			return var_name;
+		}
+		
+		public string FromNative(string var)
+		{
+			return var;
+		}
+		
+		public virtual string FromNativeReturn(string var)
+		{
+			return var;
+		}
+
+		public virtual string ToNativeReturn(string var)
+		{
+			return var;
+		}
+
+		public bool DoGenerate {
+			get {
+				return false;
+			}
+			set {
+			}
+		}
+
+		public void Generate ()
+		{
+		}
+		
+	}
+}
+
