6e44bd8cdb44cfa47f047fb91240d496ffe32176
Mike Kestner <mkestner@gmail.com>
2003-10-13 03:06:22 +0000

    2003-10-12  Mike Kestner  <mkestner@ximian.com>
    	* art/Art.metadata : new xpath metadata rules
    	* art/Makefile.in : apply metadata before generation
    	* atk/Atk.metadata : new xpath metadata rules
    	* atk/Makefile.in : apply metadata before generation
    	* gda/Gda.metadata : new xpath metadata rules
    	* gda/Makefile.in : apply metadata before generation
    	* gdk/Gdk.metadata : new xpath metadata rules
    	* gdk/Makefile.in : apply metadata before generation
    	* gnomedb/GnomeDb.metadata : new xpath metadata rules
    	* gnomedb/Makefile.in : apply metadata before generation
    	* pango/Pango.metadata : new xpath metadata rules
    	* pango/Makefile.in : apply metadata before generation
    	* parser/Makefile.in : build and install new gapi-fixup
    	* parser/gapi-fixup.cs : new xpath based metadata engine
    	* sources/*.metadata : remove most of the old metadata,
    	still have to convert Gtk and Gnome to xpaths.
    
    svn path=/trunk/gtk-sharp/; revision=18947

---
+++ parser/gapi-fixup.cs
@@ -0,0 +1,83 @@
+// GtkSharp.Parsing.gapi-fixup.cs - xml alteration engine.
+//
+// Author: Mike Kestner <mkestner@speakeasy.net>
+//
+// (c) 2003 Mike Kestner
+
+namespace GtkSharp.Parsing {
+
+	using System;
+	using System.IO;
+	using System.Xml;
+	using System.Xml.XPath;
+
+	public class Fixup  {
+
+		public static int Main (string[] args)
+		{
+			if (args.Length != 2) {
+				Console.WriteLine ("Usage: gapi-fixup --metadata=<filename> --api=<filename>");
+				return 0;
+			}
+
+			string api_filename = "";
+			XmlDocument api_doc = new XmlDocument ();
+			XmlDocument meta_doc = new XmlDocument ();
+
+			foreach (string arg in args) {
+
+				if (arg.StartsWith("--metadata=")) {
+
+					string meta_filename = arg.Substring (11);
+
+					try {
+						Stream stream = File.OpenRead (meta_filename);
+						meta_doc.Load (stream);
+						stream.Close ();
+					} catch (XmlException e) {
+						Console.WriteLine ("Invalid meta file.");
+						Console.WriteLine (e);
+						return 1;
+					}
+
+				} else if (arg.StartsWith ("--api=")) {
+
+					api_filename = arg.Substring (6);
+
+					try {
+						Stream stream = File.OpenRead (api_filename);
+						api_doc.Load (stream);
+						stream.Close ();
+					} catch (XmlException e) {
+						Console.WriteLine ("Invalid api file.");
+						Console.WriteLine (e);
+						return 1;
+					}
+
+				} else {
+					Console.WriteLine ("Usage: gapi-fixup --metadata=<filename> --api=<filename>");
+					return 1;
+				}
+			}
+
+			XPathNavigator meta_nav = meta_doc.CreateNavigator ();
+			XPathNavigator api_nav = api_doc.CreateNavigator ();
+
+			XPathNodeIterator attr_iter = meta_nav.Select ("//attr");
+			while (attr_iter.MoveNext ()) {
+				string path = attr_iter.Current.GetAttribute ("path", "");
+				string attr_name = attr_iter.Current.GetAttribute ("name", "");
+				XPathNodeIterator api_iter = api_nav.Select (path);
+				while (api_iter.MoveNext ()) {
+					XmlElement node = ((IHasXmlNode)api_iter.Current).GetNode () as XmlElement;
+					node.SetAttribute (attr_name, attr_iter.Current.Value);
+				}
+			}
+
+			Stream out_stream = File.OpenWrite (api_filename);
+			api_doc.Save (out_stream);
+			out_stream.Close ();
+			return 0;
+		}
+	}
+}
