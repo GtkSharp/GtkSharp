63887acbb5b520b3175e1ab3c68cc621edc576d4
Mike Kestner <mkestner@gmail.com>
2003-10-14 21:27:46 +0000

    2003-10-14  Mike Kestner  <mkestner@ximian.com>
    	* gtk/Gtk.metadata : rule for ClipboardGetFunc
    	* gtk/Clipboard.custom : comment out for now don't think any
    	of this is needed.
    	* gtk/ClipboardClearFunc.cs : gen'd now
    	* gtk/ClipboardGetFunc.cs : gen'd now
    	* gtk/GtkSharp.GtkClipboardClearFuncNative.cs : gen'd now
    	* gtk/GtkSharp.GtkClipboardGetFuncNative.cs : gen'd now
    	* gtk/gtk-api.xml : regenerated
    	* sources/Gtk.metadata : remove a couple clipboard hides
    
    svn path=/trunk/gtk-sharp/; revision=19048

--- gtk/ClipboardClearFunc.cs
+++
@@ -1,8 +0,0 @@
-// Generated File.  Do not modify.
-// <c> 2001-2002 Mike Kestner
-
-namespace Gtk {
-
-	public delegate void ClipboardClearFunc(Gtk.Clipboard clipboard, object o);
-
-}
--- gtk/ClipboardGetFunc.cs
+++
@@ -1,8 +0,0 @@
-// Generated File.  Do not modify.
-// <c> 2001-2002 Mike Kestner
-
-namespace Gtk {
-
-	public delegate void ClipboardGetFunc(Gtk.Clipboard clipboard, ref Gtk.SelectionData selection_data, uint info, object o);
-
-}
