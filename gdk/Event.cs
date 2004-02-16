ad8ca4e74d9c6bf3038c7e3b90ce81a8ac5e5610
Mike Kestner <mkestner@gmail.com>
2004-02-16 17:48:14 +0000

    2004-02-16  Mike Kestner  <mkestner@ximian.com>
    	* gdk/Gdk.metadata : hide EventAny
    	* gdk/Event.cs : make this a full EventAny implementation.
    	* gdk/gdk-api.xml : regen
    	* gdk/gdk-symbols.xml : map EventAny to Gdk.Event.
    	* glue/event.cs : expose window and send_event fields.
    
    svn path=/trunk/gtk-sharp/; revision=23134

--- gdk/Event.cs
+++ gdk/Event.cs
@@ -1,22 +1,48 @@
 // Gdk.Event.cs - Custom event wrapper 
 //
-// Author: Rachel Hestilow <hestilow@ximian.com> 
+// Authors: Rachel Hestilow <hestilow@ximian.com> 
+//          Mike Kestner <mkestner@ximian.com>
 //
 // (c) 2002 Rachel Hestilow
-
+// (c) 2004 Novell, Inc.
 
 namespace Gdk {
 
 	using System;
-	using System.Collections;
 	using System.Runtime.InteropServices;
 
-	public class Event : GLib.Boxed {
+	public class Event : GLib.IWrapper {
 
 		[DllImport("gtksharpglue")]
 		static extern EventType gtksharp_gdk_event_get_event_type (IntPtr evt);
 
-		public Event(IntPtr raw) : base(raw) {}
+		[DllImport("gtksharpglue")]
+		static extern IntPtr gtksharp_gdk_event_get_window (IntPtr evt);
+
+		[DllImport("gtksharpglue")]
+		static extern sbyte gtksharp_gdk_event_get_send_event (IntPtr evt);
+
+		[DllImport("libgdk-win32-2.0-0.dll")]
+		static extern IntPtr gdk_event_get_type ();
+
+		IntPtr raw;
+
+		public Event(IntPtr raw) 
+		{
+			this.raw = raw;
+		}
+
+		public IntPtr Handle {
+			get {
+				return raw;
+			}
+		}
+
+		public static GLib.GType GType {
+			get {
+				return new GLib.GType (gdk_event_get_type ());
+			}
+		}
 
 		public EventType Type {
 			get {
@@ -24,11 +50,17 @@ namespace Gdk {
 			}
 		}
 
-		public bool IsValid {
+		public Window Window {
 			get {
-				return (Handle != IntPtr.Zero);
+				return GLib.Object.GetObject (gtksharp_gdk_event_get_window (Handle)) as Window;
+			}
+		}
+
+		public bool SendEvent {
+			get {
+				return gtksharp_gdk_event_get_send_event (Handle) == 0 ? false : true;
 			}
 		}
 	}
-
 }
+
--- glue/event.c
+++ glue/event.c
@@ -1,8 +1,10 @@
 /* event.c : Glue to access fields in GdkEvent.
  *
- * Author: Rachel Hestilow  <hestilow@ximian.com>
+ * Authors: Rachel Hestilow  <hestilow@ximian.com>
+ *          Mike Kestner  <mkestner@ximian.com>
  *
  * <c> 2002 Rachel Hestilow, Mike Kestner
+ * (c) 2004 Novell, Inc.
  */
 
 
@@ -10,11 +12,25 @@
 
 /* Forward declarations */
 GdkEventType gtksharp_gdk_event_get_event_type (GdkEvent *event);
+GdkWindow* gtksharp_gdk_event_get_window (GdkEventAny *event);
+gint8 gtksharp_gdk_event_get_send_event (GdkEventAny *event);
 /* */
 
 GdkEventType
 gtksharp_gdk_event_get_event_type (GdkEvent *event)
 {
 	return event->type;
+}
+
+GdkWindow*
+gtksharp_gdk_event_get_window (GdkEventAny *event)
+{
+	return event->window;
+}
+
+gint8
+gtksharp_gdk_event_get_send_event (GdkEventAny *event)
+{
+	return event->send_event;
 }
 
