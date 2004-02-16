62c466e8b4292eb2b6ab1d9e729d46fb2dd8167a
Mike Kestner <mkestner@gmail.com>
2004-02-16 18:41:52 +0000

    2004-02-16  Mike Kestner  <mkestner@ximian.com>
    	* gdk/Gdk.metadata : hide EventKey
    	* gdk/EventKey.cs : glue-based manual implementation.
    	* gdk/EventKey.custom : killed
    	* gdk/gdk-api.xml : regen
    	* gdk/gdk-symbols.xml : manual mapping for EventKey.
    	* glue/event.cs : expose key struct fields.
    
    svn path=/trunk/gtk-sharp/; revision=23140

--- glue/event.c
+++ glue/event.c
@@ -14,6 +14,11 @@
 GdkEventType gtksharp_gdk_event_get_event_type (GdkEvent *event);
 GdkWindow* gtksharp_gdk_event_get_window (GdkEventAny *event);
 gint8 gtksharp_gdk_event_get_send_event (GdkEventAny *event);
+guint32 gtksharp_gdk_event_key_get_time (GdkEventKey *event);
+guint gtksharp_gdk_event_key_get_state (GdkEventKey *event);
+guint gtksharp_gdk_event_key_get_keyval (GdkEventKey *event);
+guint16 gtksharp_gdk_event_key_get_hardware_keycode (GdkEventKey *event);
+guint8 gtksharp_gdk_event_key_get_group (GdkEventKey *event);
 /* */
 
 GdkEventType
@@ -32,5 +37,35 @@ gint8
 gtksharp_gdk_event_get_send_event (GdkEventAny *event)
 {
 	return event->send_event;
+}
+
+guint32
+gtksharp_gdk_event_key_get_time (GdkEventKey *event)
+{
+	return event->time;
+}
+
+guint
+gtksharp_gdk_event_key_get_state (GdkEventKey *event)
+{
+	return event->time;
+}
+
+guint
+gtksharp_gdk_event_key_get_keyval (GdkEventKey *event)
+{
+	return event->keyval;
+}
+
+guint16
+gtksharp_gdk_event_key_get_hardware_keycode (GdkEventKey *event)
+{
+	return event->hardware_keycode;
+}
+
+guint8
+gtksharp_gdk_event_key_get_group (GdkEventKey *event)
+{
+	return event->group;
 }
 
