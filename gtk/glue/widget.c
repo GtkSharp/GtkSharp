369a588e93f360877533971406de632d9a4718be
Mike Kestner <mkestner@gmail.com>
2004-05-31 18:48:25 +0000

    2004-05-31  Mike Kestner  <mkestner@ximian.com>
    	* configure.in : Bump version to 0.93 and tag.
    
    2004-05-31  Jeroen Zwartepoorte  <jeroen@xs4all.nl>
    
    	* gtk/Gtk.metadata: unhide WidgetFlags.
    	* gtk/Widget.custom: add setter for GdkWindow prop. Add Flags prop.
            * gtk/glue/widget.custom: setter for window, accessors for flags.
    	[Fixes #59337]
    
    svn path=/trunk/gtk-sharp/; revision=28578

--- gtk/glue/widget.c
+++ gtk/glue/widget.c
@@ -10,6 +10,7 @@
 /* Forward declarations */
 GdkRectangle *gtksharp_gtk_widget_get_allocation (GtkWidget *widget);
 GdkWindow *gtksharp_gtk_widget_get_window (GtkWidget *widget);
+void gtksharp_gtk_widget_set_window (GtkWidget *widget, GdkWindow *window);
 int gtksharp_gtk_widget_get_state (GtkWidget *widget);
 /* */
 
@@ -25,8 +26,26 @@ gtksharp_gtk_widget_get_window (GtkWidget *widget)
 	return widget->window;
 }
 
+void
+gtksharp_gtk_widget_set_window (GtkWidget *widget, GdkWindow *window)
+{
+	widget->window = window;
+}
+
 int
 gtksharp_gtk_widget_get_state (GtkWidget *widget)
 {
 	return GTK_WIDGET_STATE (widget);
+}
+
+int
+gtksharp_gtk_widget_get_flags (GtkWidget *widget)
+{
+	return GTK_WIDGET_FLAGS (widget);
+}
+
+void
+gtksharp_gtk_widget_set_flags (GtkWidget *widget, int flags)
+{
+	GTK_WIDGET_SET_FLAGS (widget, flags);
 }
