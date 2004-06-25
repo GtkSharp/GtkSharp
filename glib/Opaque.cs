7a6d6b2128cafd0f757907d201c75a86841fb93f
Mike Kestner <mkestner@gmail.com>
2004-06-25 18:42:19 +0000

    2004-06-25  Mike Kestner  <mkestner@ximian.com>
    	* */*.cs : add lgpl license blurb and clean up (c)'s.
    	* */*.custom : add lgpl license blurb and clean up (c)'s.
    	* */glue/*.c : add lgpl license blurb and clean up (c)'s.
    	file adds without license from now on are punishable by wedgie.
    
    svn path=/trunk/gtk-sharp/; revision=30401

--- glib/Opaque.cs
+++ glib/Opaque.cs
@@ -4,7 +4,24 @@
 //	    Mike Kestner <mkestner@speakeasy.net>
 //	    Rachel Hestilow <hestilow@ximian.com>
 //
-// (c) 2001 Bob Smith and Mike Kestner, 2002 Rachel Hestilow
+// Copyright (c) 2001 Bob Smith 
+// Copyright (c) 2001 Mike Kestner
+// Copyright (c) 2002 Rachel Hestilow
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of version 2 of the Lesser GNU General 
+// Public License as published by the Free Software Foundation.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this program; if not, write to the
+// Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+// Boston, MA 02111-1307, USA.
+
 
 namespace GLib {
 
@@ -12,14 +29,6 @@ namespace GLib {
 	using System.Collections;
 	using System.ComponentModel;
 	using System.Runtime.InteropServices;
-
-	/// <summary>
-	///	Object Class
-	/// </summary>
-	///
-	/// <remarks>
-	///	Wrapper class for GObject.
-	/// </remarks>
 
 	public class Opaque : IWrapper {
 
@@ -49,14 +58,6 @@ namespace GLib {
 			if (obj != null) return obj;
 			return null; //FIXME: Call TypeParser here eventually.
 		}
-
-		/// <summary>
-		///	Opaque Constructor
-		/// </summary>
-		///
-		/// <remarks>
-		///	Dummy constructor needed for derived classes.
-		/// </remarks>
 
 		public Opaque () {}
 
--- gtk/glue/cellrenderer.c
+++ gtk/glue/cellrenderer.c
@@ -3,6 +3,20 @@
  * Author: Todd Berman (tberman@sevenl.net)
  * 
  * Copyright (C) 2004 Todd Berman
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of version 2 of the Lesser GNU General 
+ * Public License as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
  */
 
 #include <gtk/gtkcellrenderer.h>
