3eb398a12f467edceac7651ca24be9249b7bd8cb
Mike Kestner <mkestner@gmail.com>
2004-09-23 16:50:18 +0000

    2004-09-23  Mike Kestner  <mkestner@ximian.com>
    	* gtk/Widget.custom : new OnSetScrollAdjustments VM.
    	* gtk/glue/widget.c : glue for new VM.
    
    svn path=/trunk/gtk-sharp/; revision=34293

--- gtk/glue/widget.c
+++ gtk/glue/widget.c
@@ -30,6 +30,8 @@ int gtksharp_gtk_widget_get_state (GtkWidget *widget);
 int gtksharp_gtk_widget_get_flags (GtkWidget *widget);
 void gtksharp_gtk_widget_set_flags (GtkWidget *widget, int flags);
 int gtksharp_gtk_widget_style_get_int (GtkWidget *widget, const char *name);
+void gtksharp_widget_connect_set_scroll_adjustments_signal (GType gtype, gpointer callback);
+void _gtksharp_marshal_VOID__OBJECT_OBJECT (GClosure *closure, GValue *return_value, guint n_param_values, const GValue *param_values, gpointer invocation_hint, gpointer marshal_data);
 /* */
 
 GdkRectangle*
@@ -53,7 +55,7 @@ gtksharp_gtk_widget_get_window (GtkWidget *widget)
 void
 gtksharp_gtk_widget_set_window (GtkWidget *widget, GdkWindow *window)
 {
-	widget->window = window;
+	widget->window = g_object_ref (window);
 }
 
 int
@@ -81,3 +83,57 @@ gtksharp_gtk_widget_style_get_int (GtkWidget *widget, const char *name)
 	gtk_widget_style_get (widget, name, &value, NULL);
 	return value;
 }
+
+#include	<glib-object.h>
+
+#define g_marshal_value_peek_object(v)   (v)->data[0].v_pointer
+
+void
+_gtksharp_marshal_VOID__OBJECT_OBJECT (GClosure     *closure,
+                                       GValue       *return_value,
+                                       guint         n_param_values,
+                                       const GValue *param_values,
+                                       gpointer      invocation_hint,
+                                       gpointer      marshal_data)
+{
+  typedef void (*GMarshalFunc_VOID__OBJECT_OBJECT) (gpointer     data1,
+                                                    gpointer     arg_1,
+                                                    gpointer     arg_2,
+                                                    gpointer     data2);
+  register GMarshalFunc_VOID__OBJECT_OBJECT callback;
+  register GCClosure *cc = (GCClosure*) closure;
+  register gpointer data1, data2;
+
+  g_return_if_fail (n_param_values == 3);
+
+  if (G_CCLOSURE_SWAP_DATA (closure))
+    {
+      data1 = closure->data;
+      data2 = g_value_peek_pointer (param_values + 0);
+    }
+  else
+    {
+      data1 = g_value_peek_pointer (param_values + 0);
+      data2 = closure->data;
+    }
+  callback = (GMarshalFunc_VOID__OBJECT_OBJECT) (marshal_data ? marshal_data : cc->callback);
+
+  callback (data1,
+            g_marshal_value_peek_object (param_values + 1),
+            g_marshal_value_peek_object (param_values + 2),
+            data2);
+}
+
+void 
+gtksharp_widget_connect_set_scroll_adjustments_signal (GType gtype, gpointer cb)
+{
+	GType parm_types[] = {GTK_TYPE_ADJUSTMENT, GTK_TYPE_ADJUSTMENT};
+	GtkWidgetClass *klass = g_type_class_peek (gtype);
+	if (!klass)
+		klass = g_type_class_ref (gtype);
+	klass->set_scroll_adjustments_signal = g_signal_newv (
+		"set_scroll_adjustments", gtype, G_SIGNAL_RUN_LAST,
+		g_cclosure_new (cb, NULL, NULL), NULL, NULL, _gtksharp_marshal_VOID__OBJECT_OBJECT,
+		G_TYPE_NONE, 2, parm_types);
+}
+
