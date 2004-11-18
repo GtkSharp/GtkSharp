487929df7fb572f49221b64364ecca28de898a00
Mike Kestner <mkestner@gmail.com>
2004-11-18 21:51:56 +0000

    2004-11-18  Mike Kestner  <mkestner@novell.com>
    	* generator/AliasGen.cs : make this a SimpleGen.
    
    svn path=/trunk/gtk-sharp/; revision=36288

--- generator/AliasGen.cs
+++ generator/AliasGen.cs
@@ -22,79 +22,9 @@ namespace GtkSharp.Generation {
 
 	using System;
 
-	public class AliasGen : IGeneratable  {
+	public class AliasGen : SimpleGen  {
 		
-		string type;
-		string ctype;
-
-		public AliasGen (string ctype, string type)
-		{
-			this.ctype = ctype;
-			this.type = type;
-		}
-		
-		public string CName {
-			get
-			{
-				return ctype;
-			}
-		}
-
-		public string Name {
-			get
-			{
-				return type;
-			}
-		}
-
-		public string QualifiedName {
-			get
-			{
-				return type;
-			}
-		}
-
-		public string MarshalType {
-			get
-			{
-				return type;
-			}
-		}
-		public virtual string MarshalReturnType {
-			get
-			{
-				return type;
-			}
-		}
-
-		public string CallByName (string var_name)
-		{
-			return var_name;
-		}
-		
-		public string FromNative(string var)
-		{
-			return var;
-		}
-		
-		public virtual string FromNativeReturn(string var)
-		{
-			return var;
-		}
-
-		public virtual string ToNativeReturn(string var)
-		{
-			return var;
-		}
-
-		public void Generate ()
-		{
-		}
-		
-		public void Generate (GenerationInfo gen_info)
-		{
-		}
-		
+		public AliasGen (string ctype, string type) : base (ctype, type) {}
 	}
 }
 
