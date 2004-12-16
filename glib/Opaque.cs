256bc09aee6dab5cecbfb099276ff452fdeea8e2
Mike Kestner <mkestner@gmail.com>
2004-12-16 17:09:58 +0000

    2004-12-16  Tambet Ingo  <tambet@ximian.com>
    	* glib/Opaque.cs : hold a weakref in the hash, not a strong ref.
    
    svn path=/trunk/gtk-sharp/; revision=37840

--- glib/Opaque.cs
+++ glib/Opaque.cs
@@ -7,6 +7,7 @@
 // Copyright (c) 2001 Bob Smith 
 // Copyright (c) 2001 Mike Kestner
 // Copyright (c) 2002 Rachel Hestilow
+// Copyright (c) 2004 Novell, Inc.
 //
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of version 2 of the Lesser GNU General 
@@ -32,78 +33,34 @@ namespace GLib {
 
 	public class Opaque : IWrapper {
 
-		// Private class and instance members
 		IntPtr _obj;
 		static Hashtable Opaques = new Hashtable();
 
-		/// <summary>
-		///	GetObject Shared Method 
-		/// </summary>
-		///
-		/// <remarks>
-		///	Used to obtain a CLI typed object associated with a 
-		///	given raw object pointer. This method is primarily
-		///	used to wrap object references that are returned 
-		///	by either the signal system or raw class methods that
-		///	return opaque struct references.
-		/// </remarks>
-		///
-		/// <returns>
-		///	The wrapper instance.
-		/// </returns>
-
 		public static Opaque GetOpaque(IntPtr o)
 		{
-			Opaque obj = (Opaque)Opaques[o];
-			if (obj != null) return obj;
-			return null; //FIXME: Call TypeParser here eventually.
-		}
+			WeakReference reference = (WeakReference) Opaques[(int)o];
+			if (reference == null || !reference.IsAlive)
+				return null;
 
+			return (Opaque) reference.Target;
+  		}
+  
 		public Opaque () {}
-
-		/// <summary>
-		///	Opaque Constructor
-		/// </summary>
-		///
-		/// <remarks>
-		///	Creates an opaque wrapper from a raw object reference.
-		/// </remarks>
 
 		public Opaque (IntPtr raw)
 		{
 			Raw = raw;
 		}
 
-		/// <summary>
-		///	Raw Property
-		/// </summary>
-		///
-		/// <remarks>
-		///	The raw Opaque reference associated with this wrapper.
-		///	Only subclasses of Opaque can access this read/write
-		///	property.  For public read-only access, use the
-		///	Handle property.
-		/// </remarks>
-
 		protected IntPtr Raw {
 			get {
 				return _obj;
 			}
 			set {
-				Opaques [value] = this;
+				Opaques [value] = new WeakReference (this);
 				_obj = value;
 			}
 		}       
-
-		/// <summary>
-		///	Handle Property
-		/// </summary>
-		///
-		/// <remarks>
-		///	The raw Opaque reference associated with this object.
-		///	Subclasses can use Raw property for read/write
-		///	access.
-		/// </remarks>
 
 		public IntPtr Handle {
 			get {
@@ -118,14 +75,6 @@ namespace GLib {
 
 			return (Handle == ((Opaque) o).Handle);
 		}
-
-		/// <summary>
-		///	GetHashCode Method
-		/// </summary>
-		///
-		/// <remarks>
-		///	Calculates a hashing value.
-		/// </remarks>
 
 		public override int GetHashCode ()
 		{
