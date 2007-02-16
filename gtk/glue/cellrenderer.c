4183bad5cc2a3314af99eb1fa667e58a3e5f6c81
Mike Kestner <mkestner@gmail.com>
2007-02-16 18:28:37 +0000

    2007-02-16  Peter Johanson  <peter@peterjohanson.com>
    	* gtk/CellRenderer*.custom: new custom overrides for VMs.
    	* gtk/glue/cellrenderer.c: add GType parameters to the invoke methods
    	so we can identify the correct vtable to invoke from.
    
    svn path=/trunk/gtk-sharp/; revision=73037

--- gtk/glue/cellrenderer.c
+++ gtk/glue/cellrenderer.c
@@ -1,8 +1,10 @@
 /* cellrenderer.c : Glue for overriding pieces of GtkCellRenderer
  *
- * Author: Todd Berman (tberman@sevenl.net)
+ * Author: Todd Berman (tberman@sevenl.net),
+ *         Peter Johanson (peter@peterjohanson.com)
  * 
  * Copyright (C) 2004 Todd Berman
+ * Copyright (C) 2007 Peter Johanson
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of version 2 of the Lesser GNU General 
@@ -21,14 +23,22 @@
 
 #include <gtk/gtkcellrenderer.h>
 
+void gtksharp_cellrenderer_invoke_get_size (GType gtype, GtkCellRenderer *cell, GtkWidget *widget, GdkRectangle *cell_area, gint *x_offset, gint *y_offset, gint *width, gint *height);
+
+void
+gtksharp_cellrenderer_invoke_get_size (GType type, GtkCellRenderer *cell, GtkWidget *widget, GdkRectangle *cell_area, gint *x_offset, gint *y_offset, gint *width, gint *height)
+{
+	GtkCellRendererClass *klass = g_type_class_peek (type);
+	klass->get_size (cell, widget, cell_area, x_offset, y_offset, width, height);
+}
+
 void gtksharp_cellrenderer_base_get_size (GtkCellRenderer *cell, GtkWidget *widget, GdkRectangle *cell_area, gint *x_offset, gint *y_offset, gint *width, gint *height);
 
 void
 gtksharp_cellrenderer_base_get_size (GtkCellRenderer *cell, GtkWidget *widget, GdkRectangle *cell_area, gint *x_offset, gint *y_offset, gint *width, gint *height)
 {
-	GtkCellRendererClass *parent = g_type_class_peek_parent (G_OBJECT_GET_CLASS (cell));
-	if (parent->get_size)
-		(*parent->get_size) (cell, widget, cell_area, x_offset, y_offset, width, height);
+	if (GTK_CELL_RENDERER_GET_CLASS (cell)->get_size)
+		GTK_CELL_RENDERER_GET_CLASS (cell)->get_size (cell, widget, cell_area, x_offset, y_offset, width, height);
 }
 
 void gtksharp_cellrenderer_override_get_size (GType gtype, gpointer cb);
@@ -42,14 +52,22 @@ gtksharp_cellrenderer_override_get_size (GType gtype, gpointer cb)
 	((GtkCellRendererClass *) klass)->get_size = cb;
 }
 
+void gtksharp_cellrenderer_invoke_render (GType type, GtkCellRenderer *cell, GdkDrawable *window, GtkWidget *widget, GdkRectangle *background_area, GdkRectangle *cell_area, GdkRectangle *expose_area, GtkCellRendererState flags);
+
+void
+gtksharp_cellrenderer_invoke_render (GType type, GtkCellRenderer *cell, GdkDrawable *window, GtkWidget *widget, GdkRectangle *background_area, GdkRectangle *cell_area, GdkRectangle *expose_area, GtkCellRendererState flags)
+{
+	GtkCellRendererClass *klass = g_type_class_peek (type);
+	klass->render (cell, window, widget, background_area, cell_area, expose_area, flags);
+}
+
 void gtksharp_cellrenderer_base_render (GtkCellRenderer *cell, GdkDrawable *window, GtkWidget *widget, GdkRectangle *background_area, GdkRectangle *cell_area, GdkRectangle *expose_area, GtkCellRendererState flags);
 
 void
 gtksharp_cellrenderer_base_render (GtkCellRenderer *cell, GdkDrawable *window, GtkWidget *widget, GdkRectangle *background_area, GdkRectangle *cell_area, GdkRectangle *expose_area, GtkCellRendererState flags)
 {
-	GtkCellRendererClass *parent = g_type_class_peek_parent (G_OBJECT_GET_CLASS (cell));
-	if (parent->render)
-		(*parent->render) (cell, window, widget, background_area, cell_area, expose_area, flags);
+	if (GTK_CELL_RENDERER_GET_CLASS (cell)->render)
+		GTK_CELL_RENDERER_GET_CLASS (cell)->render (cell, window, widget, background_area, cell_area, expose_area, flags);
 }
 
 void gtksharp_cellrenderer_override_render (GType gtype, gpointer cb);
@@ -63,14 +81,22 @@ gtksharp_cellrenderer_override_render (GType gtype, gpointer cb)
 	((GtkCellRendererClass *) klass)->render = cb;
 }
 
+GtkCellEditable * gtksharp_cellrenderer_invoke_start_editing (GType type, GtkCellRenderer *cell, GdkEvent *event, GtkWidget *widget, const gchar *path, GdkRectangle *background_area, GdkRectangle *cell_area, GtkCellRendererState flags);
+
+GtkCellEditable *
+gtksharp_cellrenderer_invoke_start_editing (GType type, GtkCellRenderer *cell, GdkEvent *event, GtkWidget *widget, const gchar *path, GdkRectangle *background_area, GdkRectangle *cell_area, GtkCellRendererState flags)
+{
+	GtkCellRendererClass *klass = g_type_class_peek (type);
+	klass->start_editing (cell, event, widget, path, background_area, cell_area, flags);
+}
+
 GtkCellEditable * gtksharp_cellrenderer_base_start_editing (GtkCellRenderer *cell, GdkEvent *event, GtkWidget *widget, const gchar *path, GdkRectangle *background_area, GdkRectangle *cell_area, GtkCellRendererState flags);
 
 GtkCellEditable *
 gtksharp_cellrenderer_base_start_editing (GtkCellRenderer *cell, GdkEvent *event, GtkWidget *widget, const gchar *path, GdkRectangle *background_area, GdkRectangle *cell_area, GtkCellRendererState flags)
 {
-        GtkCellRendererClass *parent = g_type_class_peek_parent (G_OBJECT_GET_CLASS (cell));
-        if (parent->start_editing)
-		return (*parent->start_editing) (cell, event, widget, path, background_area, cell_area, flags);
+	if (GTK_CELL_RENDERER_GET_CLASS (cell)->start_editing)
+		return GTK_CELL_RENDERER_GET_CLASS (cell)->start_editing (cell, event, widget, path, background_area, cell_area, flags);
 	return NULL;	
 }
 
