2110dad6d1d028f873788fb2bed50af288a7c834
Mike Kestner <mkestner@gmail.com>
2009-01-08 20:05:24 +0000

    2009-01-08  Mike Kestner  <mkestner@novell.com>
    	* cairo/Makefile.am: use mono.snk to sign the Mono.Cairo assembly
    	so that it's compatible with mono built versions.  Initial patch
    	from Christian Hoff with a few tweaks.
    
    svn path=/trunk/gtk-sharp/; revision=122821

---
+++ cairo/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly:AssemblyVersion("2.0.0.0")]
+[assembly:AssemblyDelaySign(false)]
+[assembly:AssemblyKeyFile("mono.snk")]
