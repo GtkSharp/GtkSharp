a62a57d8773b4ff916b86dd24f0209b4518cb36b
Stephane Delcroix <stephane@mono-cvs.ximian.com>
2009-02-11 15:07:42 +0000

    new sample for VolumeMonitor, Volume, ...
    svn path=/trunk/gtk-sharp/; revision=126610

---
+++ sample/gio/Volume.cs
@@ -0,0 +1,31 @@
+using GLib;
+using System;
+
+namespace TestGio
+{
+	public class TestVolume
+	{
+		static void Main (string[] args)
+		{
+			GLib.GType.Init ();
+			VolumeMonitor monitor = VolumeMonitor.Default;
+			Console.WriteLine ("Volumes:");
+			foreach (Volume v in monitor.Volumes)
+				Console.WriteLine ("\t{0}", v);
+			Console.WriteLine ("\nMounts:");
+			foreach (Mount m in monitor.Mounts) {
+				Console.WriteLine ("\tName:{0}, UUID:{1}, root:{2}, CanUnmount: {3}", m.Name, m.Uuid, m.Root, m.CanUnmount);
+				Volume v = m.Volume;
+				if (v != null)
+					Console.WriteLine ("\t\tVolume:{0}", v.Name);
+				Drive d = m.Drive;
+				if (d != null)
+					Console.WriteLine ("\t\tDrive:{0}", d.Name);
+			}
+			Console.WriteLine ("\nConnectedDrives:");
+			foreach (object o in monitor.ConnectedDrives)
+				Console.WriteLine ("\t{0}", o);
+		}
+	}
+}
+
