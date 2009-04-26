39739ae746be91468c9abff8754d9b11a7c895b3
Mike Kestner <mkestner@gmail.com>
2009-04-26 03:21:47 +0000

    2009-04-25  Mike Kestner  <mkestner@novell.com>
    	* generator/OpaqueGen.cs: support abstract opaque classes,
    	though I have no idea why anyone would want one.
    	[Fixes #494212] Revised patch from Sebastian Dröge.
    
    svn path=/trunk/gtk-sharp/; revision=132678

--- generator/OpaqueGen.cs
+++ generator/OpaqueGen.cs
@@ -63,7 +63,7 @@ namespace GtkSharp.Generation {
 
 			if (IsDeprecated)
 				sw.WriteLine ("\t[Obsolete]");
-			sw.Write ("\t{0} class " + Name, IsInternal ? "internal" : "public");
+			sw.Write ("\t{0}{1}class " + Name, IsInternal ? "internal " : "public ", IsAbstract ? "abstract " : String.Empty);
 			string cs_parent = table.GetCSType(Elem.GetAttribute("parent"));
 			if (cs_parent != "")
 				sw.Write (" : " + cs_parent);
