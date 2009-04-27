e886e07a1fb7b02ce2393748b395d316cec1d060
Christian Hoff <choff@mono-cvs.ximian.com>
2009-04-27 17:55:30 +0000

    2009-04-27  Christian Hoff  <christian_hoff@gmx.net>
    	* gtk/Widget.custom: Implement signal registration for the
    	Activate, SettScrollAdjustments and key binding signals
     	in managed code.
    
    svn path=/trunk/gtk-sharp/; revision=132771

--- gtk/glue/widget.c
+++ gtk/glue/widget.c
@@ -32,14 +32,10 @@ int gtksharp_gtk_widget_get_state (GtkWidget *widget);
 int gtksharp_gtk_widget_get_flags (GtkWidget *widget);
 void gtksharp_gtk_widget_set_flags (GtkWidget *widget, int flags);
 int gtksharp_gtk_widget_style_get_int (GtkWidget *widget, const char *name);
-void gtksharp_widget_connect_set_scroll_adjustments_signal (GType gtype, gpointer callback);
-void gtksharp_widget_connect_activate_signal (GType gtype, gpointer callback);
-void _gtksharp_marshal_VOID__OBJECT_OBJECT (GClosure *closure, GValue *return_value, guint n_param_values, const GValue *param_values, gpointer invocation_hint, gpointer marshal_data);
+void gtksharp_widget_class_set_set_scroll_adjustments_signal (GtkWidgetClass *klass, guint signal_id);
+void gtksharp_widget_class_set_activate_signal (GtkWidgetClass *klass, guint signal_id);
 int gtksharp_gtk_widget_get_flags (GtkWidget *widget);
 void gtksharp_gtk_widget_set_flags (GtkWidget *widget, int flags);
-int gtksharp_gtk_widget_style_get_int (GtkWidget *widget, const char *name);
-void gtksharp_widget_add_binding_signal (GType gtype, const char *sig_name, GCallback cb);
-void gtksharp_widget_register_binding (GType gtype, const char *sig_name, guint key, int mod, gpointer data);
 /* */
 
 GdkRectangle*
@@ -80,92 +76,15 @@ gtksharp_gtk_widget_set_flags (GtkWidget *widget, int flags)
 	GTK_OBJECT(widget)->flags = flags;
 }
 
-int
-gtksharp_gtk_widget_style_get_int (GtkWidget *widget, const char *name)
+void 
+gtksharp_widget_class_set_set_scroll_adjustments_signal (GtkWidgetClass *klass, guint signal_id)
 {
-	int value;
-	gtk_widget_style_get (widget, name, &value, NULL);
-	return value;
-}
-
-#include	<glib-object.h>
-
-#define g_marshal_value_peek_object(v)   (v)->data[0].v_pointer
-
-void
-_gtksharp_marshal_VOID__OBJECT_OBJECT (GClosure     *closure,
-                                       GValue       *return_value,
-                                       guint         n_param_values,
-                                       const GValue *param_values,
-                                       gpointer      invocation_hint,
-                                       gpointer      marshal_data)
-{
-  typedef void (*GMarshalFunc_VOID__OBJECT_OBJECT) (gpointer     data1,
-                                                    gpointer     arg_1,
-                                                    gpointer     arg_2,
-                                                    gpointer     data2);
-  register GMarshalFunc_VOID__OBJECT_OBJECT callback;
-  register GCClosure *cc = (GCClosure*) closure;
-  register gpointer data1, data2;
-
-  g_return_if_fail (n_param_values == 3);
-
-  if (G_CCLOSURE_SWAP_DATA (closure))
-    {
-      data1 = closure->data;
-      data2 = g_value_peek_pointer (param_values + 0);
-    }
-  else
-    {
-      data1 = g_value_peek_pointer (param_values + 0);
-      data2 = closure->data;
-    }
-  callback = (GMarshalFunc_VOID__OBJECT_OBJECT) (marshal_data ? marshal_data : cc->callback);
-
-  callback (data1,
-            g_marshal_value_peek_object (param_values + 1),
-            g_marshal_value_peek_object (param_values + 2),
-            data2);
+	klass->set_scroll_adjustments_signal = signal_id;
 }
 
 void 
-gtksharp_widget_connect_set_scroll_adjustments_signal (GType gtype, gpointer cb)
+gtksharp_widget_class_set_activate_signal (GtkWidgetClass *klass, guint signal_id)
 {
-	GType parm_types[] = {GTK_TYPE_ADJUSTMENT, GTK_TYPE_ADJUSTMENT};
-	GtkWidgetClass *klass = g_type_class_peek (gtype);
-	if (!klass)
-		klass = g_type_class_ref (gtype);
-	klass->set_scroll_adjustments_signal = g_signal_newv (
-		"set_scroll_adjustments", gtype, G_SIGNAL_RUN_LAST,
-		g_cclosure_new (cb, NULL, NULL), NULL, NULL, _gtksharp_marshal_VOID__OBJECT_OBJECT,
-		G_TYPE_NONE, 2, parm_types);
+	klass->activate_signal = signal_id;
 }
 
-void 
-gtksharp_widget_connect_activate_signal (GType gtype, gpointer cb)
-{
-	GtkWidgetClass *klass = g_type_class_peek (gtype);
-	if (!klass)
-		klass = g_type_class_ref (gtype);
-	klass->activate_signal = g_signal_newv (
-		"activate_signal", gtype, G_SIGNAL_RUN_LAST,
-		g_cclosure_new (cb, NULL, NULL), NULL, NULL, g_cclosure_marshal_VOID__VOID,
-		G_TYPE_NONE, 0, NULL);
-}
-
-void
-gtksharp_widget_add_binding_signal (GType gtype, const gchar *sig_name, GCallback cb)
-{
-	GType parm_types[] = {G_TYPE_LONG};
-	g_signal_newv (sig_name, gtype, G_SIGNAL_RUN_LAST | G_SIGNAL_ACTION, g_cclosure_new (cb, NULL, NULL), NULL, NULL, g_cclosure_marshal_VOID__LONG, G_TYPE_NONE, 1, parm_types);
-}
-
-void
-gtksharp_widget_register_binding (GType gtype, const gchar *signame, guint key, int mod, gpointer data)
-{
-	GObjectClass *klass = g_type_class_peek (gtype);
-	if (klass == NULL)
-		klass = g_type_class_ref (gtype);
-	GtkBindingSet *set = gtk_binding_set_by_class (klass);
-	gtk_binding_entry_add_signal (set, key, mod, signame, 1, G_TYPE_LONG, data);
-}
