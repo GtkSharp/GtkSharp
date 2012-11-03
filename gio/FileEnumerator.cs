24b0e12c62ef99cf7dfcf96f01912a38e5741918
Bertrand Lorentz <bertrand.lorentz@gmail.com>
2012-11-03 17:57:31 +0100

    gio: Make FileEnumerator implement IEnumerable<FileInfo>

--- gio/FileEnumerator.cs
+++ gio/FileEnumerator.cs
@@ -21,34 +21,47 @@
 namespace GLib {
 	using System;
 	using System.Collections;
+	using System.Collections.Generic;
 	
-	public partial class FileEnumerator {
-		public IEnumerator GetEnumerator ()
+	public partial class FileEnumerator : IEnumerable<FileInfo> {
+		public IEnumerator<FileInfo> GetEnumerator ()
 		{
 			return new Enumerator (this);
 		}
-		
+
+		IEnumerator IEnumerable.GetEnumerator ()
+		{
+			return GetEnumerator ();
+		}
+
 		public FileInfo NextFile ()
 		{
 			return NextFile ((Cancellable) null);
 		}
-		
-		class Enumerator : IEnumerator
+
+		class Enumerator : IEnumerator<FileInfo>
 		{
 			FileEnumerator file_enumerator;
-		
+
 			public Enumerator (FileEnumerator file_enumerator)
 			{
 				this.file_enumerator = file_enumerator;
 			}
-		
-			FileInfo current=null;
-			public object Current {
+
+			void IDisposable.Dispose() { }
+
+			FileInfo current;
+			public FileInfo Current {
 				get {
 					return current;
 				}
 			}
-		
+
+			object IEnumerator.Current
+			{
+				get { return Current; }
+			}
+
 			public bool MoveNext ()
 			{
 				current = file_enumerator.NextFile ();
