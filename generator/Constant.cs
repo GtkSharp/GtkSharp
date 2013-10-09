e031a4ff18996d8a108bd98081cd175a8f966f38
Stephan Sundermann <stephansundermann@gmail.com>
2013-10-09 18:51:38 +0200

    generator: auto escape string constants

--- generator/Constant.cs
+++ generator/Constant.cs
@@ -58,7 +58,13 @@ namespace GtkSharp.Generation
 		{
 			StreamWriter sw = gen_info.Writer;
 
-			sw.WriteLine ("{0}public const {1} {2} = {3}{4}{3};", indent, ConstType, Name, IsString ? "\"": String.Empty, value);
+			sw.WriteLine ("{0}public const {1} {2} = {3}{4}{5};",
+			              indent,
+			              ConstType,
+			              Name,
+			              IsString ? "@\"": String.Empty,
+			              value,
+			              IsString ? "\"": String.Empty);
 		}
 	}
 }
