c45e3558b6b871f28f8c774734b6b740caf67258
grbd <garlicbready@googlemail.com>
2016-10-17 20:05:48 +0100

    Rewrite of the nuget package genertion code, also included the glue libs

--- NuGet/build.py
+++ NuGet/build.py
@@ -1,20 +1,17 @@
 #!/usr/bin/python3
 """Script to build out the .Net dll's and package them into a Nuget Package for gtksharp3"""
 import os, sys
-from pybuild.GtkSharp_Builder import GtkSharp_Builder
-from pybuild.Gtk_Builder import Gtk_Builder
-from pybuild.Helper import Helper as helpers
+from pybuild.profiles.GtkSharp import GtkSharp
+from pybuild.profiles.Glue_Win32 import Glue_Win32
+from pybuild.profiles.Glue_Win64 import Glue_Win64
+from pybuild.profiles.Gtk_Win32 import Gtk_Win32
+from pybuild.profiles.Gtk_Win64 import Gtk_Win64
 
 # Ideally I'd like to see the GtkSharp Build system redone via .Net Core or something other than make
 # For now though we rely on the use of make to build the .Net dll's
 # under linux we run this natively, under windows we can use MSYS2
 
 class Build(object):
-
-    # Class Init
-    def __init__(self):
-        self.GtkSharp_Builder = GtkSharp_Builder()
-        self.Gtk_Builder = Gtk_Builder()
 
     # Clean the Build directory
     def clean(self):
@@ -26,34 +23,56 @@ class Build(object):
     def usage(self):
         print ("Please use GtkSharp3_Build.py <target> where <target> is one of")
         print ("  clean                 to clean the output directory: ./build")
-        print ("  gtksharp_net45        to build ,Net libs for GtkSharp, via .Net 4.5")
-        print ("  gtksharp_nuget_net45  to build Nuget Packages for GtkSharp, via .Net 4.5")
-        print ("  gtk_nuget_win32       to build the Nuget package for GtkSharp.Win32")
-        print ("  gtk_nuget_win64     to build the Nuget package for GtkSharp.Win64")
-        print ("  all                 to make all")
+
+#        print ("  gtksharp_all          to build .Net libs for GtkSharp, via .Net 4.5 and .Net Core")
+        print ("  gtksharp_net45        to build .Net libs for GtkSharp, via .Net 4.5")
+#        print ("  gtksharp_core         to build .Net libs for GtkSharp, via .Net Core")
+
+        print ("  gtk_win32             to build the Nuget package for GtkSharp.Win32")
+        print ("  gtk_win64             to build the Nuget package for GtkSharp.Win64")
+        print ("  all                   to make all")
 
     def main(self):
         if len(sys.argv) != 2:
             self.usage()
             return
 
-        if sys.argv[1] == "gtksharp_net45":
-            self.GtkSharp_Builder.build_net45()
-        if sys.argv[1] == "gtksharp_nuget_net45":
-            self.GtkSharp_Builder.build_nuget_net45()
-        if sys.argv[1] == "gtk_nuget_win32":
-            self.Gtk_Builder.build_nuget_win32()
-        if sys.argv[1] == "gtk_nuget_win64":
-            self.Gtk_Builder.build_nuget_win64()
+        if sys.argv[1] == 'all':
+            self.runbuild('gtksharp_net45')
+            self.runbuild('gtk_win32')
+            self.runbuild('gtk_win64')
+            return
 
-        if sys.argv[1] == "all":
-            self.GtkSharp_Builder.build_net45()
-            self.GtkSharp_Builder.build_nuget_net45()
-            self.Gtk_Builder.build_nuget_win32()
-            self.Gtk_Builder.build_nuget_win64()
+        self.runbuild(sys.argv[1])
 
-        if sys.argv[1] == "clean":
+
+    def runbuild(self, build_type):
+        
+        if build_type == 'clean':
             self.clean()
+
+        elif build_type == 'gtksharp_net45':
+            profile = GtkSharp()
+            profile.clean()
+            profile.build_net45()
+            profile.copy_net45()
+            profile.build_nuget()
+
+        elif build_type == 'gtk_win32':
+            profile_glue = Glue_Win32()
+            profile_glue.clean()
+            profile_glue.build()
+            profile_gtk = Gtk_Win32()
+            profile_gtk.build()
+            profile_gtk.build_nuget()
+
+        elif build_type == 'gtk_win64':
+            profile_glue = Glue_Win64()
+            profile_glue.clean()
+            profile_glue.build()
+            profile = Gtk_Win64()
+            profile.build()
+            profile.build_nuget()
 
 if __name__ == "__main__":
     Build().main()
---
+++ NuGet/pybuild/profiles/Glue_Win32.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+
+import os, shutil, ntpath
+from pybuild.ProfileBase import ProfileBase
+from os.path import abspath, join
+from glob import glob
+from pybuild.Helper import Helper
+from pybuild.profiles.GtkSharp import GtkSharp
+
+class Glue_Win32(ProfileBase):
+
+    def __init__(self):
+        """Class Init"""
+        super().__init__()
+        self._NuGet_PackageName = 'GtkSharp.Win32.Glue'
+        self._Version = Helper.get_gtksharp_version(self.SrcDir)
+        self.MSYSTEM = 'MINGW32'
+
+    def Get_Dlls_GtkSharp_Glue(self):
+        ret = []
+
+        # Gtksharp Glue libs
+        ret.append(['atk/glue/.libs/*atksharpglue-3.dll', 'atksharpglue-3.dl_'])
+        ret.append(['pango/glue/.libs/*pangosharpglue-3.dll', 'pangosharpglue-3.dl_'])
+        ret.append(['gio/glue/.libs/*giosharpglue-3.dll', 'giosharpglue-3.dl_'])
+        ret.append(['gtk/glue/.libs/*gtksharpglue-3.dll', 'gtksharpglue-3.dl_'])
+        return ret
+
+    def build(self):
+        """Package up a nuget file based on the default build"""
+
+        if os.name != 'nt':
+           print("Skipping Native Nuget package build, as this needs to be run on Windows")
+           return
+
+        # Trigger build of gtksharp with specific bash for Mingw32
+        builder = GtkSharp()
+        builder.MSYSTEM = self.MSYSTEM
+        builder.build_net45()
+
+        net45_build_dir = join(self.Build_NugetDir, 'build', 'net45')
+        os.makedirs(net45_build_dir, exist_ok=True)
+
+        print ('Copying Files')
+        dll_list = self.Get_Dlls_GtkSharp_Glue()
+
+        for item in dll_list:
+            src = glob(abspath(join(self.SrcDir, item[0])))[0]
+            f_basename, f_extension = os.path.splitext(ntpath.basename(src))
+            dest = join(net45_build_dir, item[1])
+            shutil.copy(src, dest)
