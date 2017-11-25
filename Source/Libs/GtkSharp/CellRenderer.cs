90bbc67f3c771ac1b72275c1ac47c60752d165a6
cra0zy <cra0zy@gmail.com>
2017-11-25 02:01:55 +0100

    Remove static glue code

--- Source/Libs/GtkSharp/CellRenderer.cs
+++ Source/Libs/GtkSharp/CellRenderer.cs
@@ -52,57 +52,5 @@ namespace Gtk {
 		// TODO: Implement "nullable" attribute for value type parameters in GAPI
 		[UnmanagedFunctionPointer (CallingConvention.Cdecl)]
 		delegate void OnGetSizeDelegate (IntPtr item, IntPtr widget, IntPtr cell_area_ptr, IntPtr x_offset, IntPtr y_offset, IntPtr width, IntPtr height);
-
-		static void OnGetSize_cb (IntPtr item, IntPtr widget, IntPtr cell_area_ptr, IntPtr x_offset, IntPtr y_offset, IntPtr width, IntPtr height)
-		{
-			try {
-				CellRenderer obj = GLib.Object.GetObject (item, false) as CellRenderer;
-				Gtk.Widget widg = GLib.Object.GetObject (widget, false) as Gtk.Widget;
-				Gdk.Rectangle cell_area = Gdk.Rectangle.Zero;
-				if (cell_area_ptr != IntPtr.Zero)
-					cell_area = Gdk.Rectangle.New (cell_area_ptr);
-				int a, b, c, d;
-
-				obj.OnGetSize (widg, ref cell_area, out a, out b, out c, out d);
-				if (x_offset != IntPtr.Zero)
-					Marshal.WriteInt32 (x_offset, a);
-				if (y_offset != IntPtr.Zero)
-					Marshal.WriteInt32 (y_offset, b);
-				if (width != IntPtr.Zero)
-					Marshal.WriteInt32 (width, c);
-				if (height != IntPtr.Zero)
-					Marshal.WriteInt32 (height, d);
-			} catch (Exception e) {
-				GLib.ExceptionManager.RaiseUnhandledException (e, false);
-			}
-		}
-
-		[DllImport("gtksharpglue-3")]
-		static extern void gtksharp_cellrenderer_override_get_size (IntPtr gtype, OnGetSizeDelegate cb);
-
-		static OnGetSizeDelegate OnGetSizeCallback;
-		static void OverrideOnGetSize (GLib.GType gtype)
-		{
-			if (OnGetSizeCallback == null)
-				OnGetSizeCallback = new OnGetSizeDelegate (OnGetSize_cb);
-			gtksharp_cellrenderer_override_get_size (gtype.Val, OnGetSizeCallback);
-		}
-
-		[GLib.DefaultSignalHandler (Type=typeof(Gtk.CellRenderer), ConnectionMethod="OverrideOnGetSize")] 
-		protected virtual void OnGetSize (Gtk.Widget widget, ref Gdk.Rectangle cell_area, out int x_offset, out int y_offset, out int width, out int height) 
-		{
-			InternalOnGetSize (widget, ref cell_area, out x_offset, out y_offset, out width, out height);
-		}
-
-		[DllImport("gtksharpglue-3")]
-		static extern void gtksharp_cellrenderer_base_get_size (IntPtr cell, IntPtr widget, IntPtr cell_area, out int x_offset, out int y_offset, out int width, out int height);
-
-		private void InternalOnGetSize (Gtk.Widget widget, ref Gdk.Rectangle cell_area, out int x_offset, out int y_offset, out int width, out int height) 
-		{
-			IntPtr native_cell_area = GLib.Marshaller.StructureToPtrAlloc (cell_area);
-			gtksharp_cellrenderer_base_get_size (Handle, widget == null ? IntPtr.Zero : widget.Handle, native_cell_area, out x_offset, out y_offset, out width, out height);
-			cell_area = Gdk.Rectangle.New (native_cell_area);
-			Marshal.FreeHGlobal (native_cell_area);
-		}
 	}
 }
--- Source/Libs/GtkSharp/Container.cs
+++ Source/Libs/GtkSharp/Container.cs
@@ -18,244 +18,211 @@
 // Free Software Foundation, Inc., 59 Temple Place - Suite 330,
 // Boston, MA 02111-1307, USA.
 
-namespace Gtk {
+namespace Gtk
+{
 
-	using System;
-	using System.Collections;
-	using System.Runtime.InteropServices;
+    using System;
+    using System.Collections;
+    using System.Runtime.InteropServices;
 
-	public partial class Container : IEnumerable {
+    public partial class Container : IEnumerable
+    {
+        [DllImport(Global.GtkNativeDll)]
+        static extern GParamSpec gtk_container_class_find_child_property (IntPtr cclass, string property_name);
 
-		[DllImport("gtksharpglue-3")]
-		static extern void gtksharp_container_child_get_property (IntPtr container, IntPtr child, IntPtr property, ref GLib.Value value);
+        [DllImport(Global.GtkNativeDll)]
+        static extern void gtk_container_child_get_property(IntPtr container, IntPtr child, string property_name, ref GLib.Value value);
 
-		public GLib.Value ChildGetProperty (Gtk.Widget child, string property_name) {
-			GLib.Value value = new GLib.Value ();
+        struct GTypeInstance
+        {
+            public IntPtr GTypeClass;
+        }
 
-			IntPtr native = GLib.Marshaller.StringToPtrGStrdup (property_name);
-			gtksharp_container_child_get_property (Handle, child.Handle, native, ref value);
-			GLib.Marshaller.Free (native);
-			return value;
-		}
+        struct GParamSpec
+        {
+            public GTypeInstance g_type_instance;
 
-		public IEnumerator GetEnumerator ()
-		{
-			return Children.GetEnumerator ();
-		}
+            public IntPtr name;
+            public int flags;
+            public GLib.GType value_type;
+            public GLib.GType owner_type;
 
-		class ChildAccumulator {
-			public ArrayList Children = new ArrayList ();
+            IntPtr _nick;
+            IntPtr _blurb;
+            IntPtr qdata;
+            uint ref_count;
+            uint param_id;
+        };
 
-			public void Add (Gtk.Widget widget)
-			{
-				Children.Add (widget);
-			}
-		}
+        public GLib.Value ChildGetProperty(Gtk.Widget child, string property_name)
+        {
+            var value = new GLib.Value();
+            var cclass = ((GTypeInstance)Marshal.PtrToStructure(Handle, typeof(GTypeInstance))).GTypeClass;
+            var prop = gtk_container_class_find_child_property(cclass, property_name);
 
-		public IEnumerable AllChildren {
-			get {
-				ChildAccumulator acc = new ChildAccumulator ();
-				Forall (new Gtk.Callback (acc.Add));
-				return acc.Children;
-			}
-		}
+            value.Init(prop.value_type);
+            gtk_container_child_get_property(Handle, child.Handle, property_name, ref value);
 
-		[DllImport (Global.GtkNativeDll, CallingConvention = CallingConvention.Cdecl)]
-		static extern bool gtk_container_get_focus_chain (IntPtr raw, out IntPtr list_ptr);
+            return value;
+        }
 
-		[DllImport (Global.GtkNativeDll, CallingConvention = CallingConvention.Cdecl)]
-		static extern void gtk_container_set_focus_chain (IntPtr raw, IntPtr list_ptr);
+        public IEnumerator GetEnumerator()
+        {
+            return Children.GetEnumerator();
+        }
 
-		public Widget[] FocusChain {
-			get {
-				IntPtr list_ptr;
-				bool success = gtk_container_get_focus_chain (Handle, out list_ptr);
-				if (!success)
-					return new Widget [0];
+        class ChildAccumulator
+        {
+            public ArrayList Children = new ArrayList();
 
-				GLib.List list = new GLib.List (list_ptr);
-				Widget[] result = new Widget [list.Count];
-				for (int i = 0; i < list.Count; i++)
-					result [i] = list [i] as Widget;
-				return result;
-			}
-			set {
-				GLib.List list = new GLib.List (IntPtr.Zero);
-				foreach (Widget val in value)
-					list.Append (val.Handle);
-				gtk_container_set_focus_chain (Handle, list.Handle);
-			}
+            public void Add(Gtk.Widget widget)
+            {
+                Children.Add(widget);
+            }
+        }
 
-		}
+        public IEnumerable AllChildren
+        {
+            get
+            {
+                ChildAccumulator acc = new ChildAccumulator();
+                Forall(new Gtk.Callback(acc.Add));
+                return acc.Children;
+            }
+        }
 
-		[DllImport("gtksharpglue-3")]
-		static extern void gtksharp_container_base_forall (IntPtr handle, bool include_internals, IntPtr cb, IntPtr data);
+        [DllImport(Global.GtkNativeDll, CallingConvention = CallingConvention.Cdecl)]
+        static extern bool gtk_container_get_focus_chain(IntPtr raw, out IntPtr list_ptr);
 
-		[DllImport("gtksharpglue-3")]
-		static extern void gtksharp_container_override_forall (IntPtr gtype, ForallDelegate cb);
+        [DllImport(Global.GtkNativeDll, CallingConvention = CallingConvention.Cdecl)]
+        static extern void gtk_container_set_focus_chain(IntPtr raw, IntPtr list_ptr);
 
-		[DllImport("gtksharpglue-3")]
-		static extern void gtksharp_container_invoke_gtk_callback (IntPtr cb, IntPtr handle, IntPtr data);
+        public Widget[] FocusChain
+        {
+            get
+            {
+                IntPtr list_ptr;
+                bool success = gtk_container_get_focus_chain(Handle, out list_ptr);
+                if (!success)
+                    return new Widget[0];
 
-		[UnmanagedFunctionPointer (CallingConvention.Cdecl)]
-		delegate void ForallDelegate (IntPtr container, bool include_internals, IntPtr cb, IntPtr data);
+                GLib.List list = new GLib.List(list_ptr);
+                Widget[] result = new Widget[list.Count];
+                for (int i = 0; i < list.Count; i++)
+                    result[i] = list[i] as Widget;
+                return result;
+            }
+            set
+            {
+                GLib.List list = new GLib.List(IntPtr.Zero);
+                foreach (Widget val in value)
+                    list.Append(val.Handle);
+                gtk_container_set_focus_chain(Handle, list.Handle);
+            }
 
-		static ForallDelegate ForallOldCallback;
-		static ForallDelegate ForallCallback;
+        }
 
-		public struct CallbackInvoker {
-			IntPtr cb;
-			IntPtr data;
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        delegate void ForallDelegate(IntPtr container, bool include_internals, IntPtr cb, IntPtr data);
 
-			internal CallbackInvoker (IntPtr cb, IntPtr data)
-			{
-				this.cb = cb;
-				this.data = data;
-			}
+        static ForallDelegate ForallOldCallback;
+        static ForallDelegate ForallCallback;
 
-			internal IntPtr Data {
-				get {
-					return data;
-				}
-			}
+        public struct CallbackInvoker
+        {
+            IntPtr cb;
+            IntPtr data;
 
-			internal IntPtr Callback {
-				get {
-					return cb;
-				}
-			}
+            internal CallbackInvoker(IntPtr cb, IntPtr data)
+            {
+                this.cb = cb;
+                this.data = data;
+            }
 
-			public void Invoke (Widget w)
-			{
-				gtksharp_container_invoke_gtk_callback (cb, w.Handle, data);
-			}
-		}
+            internal IntPtr Data
+            {
+                get
+                {
+                    return data;
+                }
+            }
 
-		static void ForallOld_cb (IntPtr container, bool include_internals, IntPtr cb, IntPtr data)
-		{
-			try {
-				//GtkContainer's unmanaged dispose calls forall, but by that time the managed object is gone
-				//so it couldn't do anything useful, and resurrecting it would cause a resurrection cycle.
-				//In that case, just chain to the native base in case it can do something.
-				Container obj = (Container) GLib.Object.TryGetObject (container);
-				if (obj != null) {
-					CallbackInvoker invoker = new CallbackInvoker (cb, data);
-					obj.ForAll (include_internals, invoker);
-				} else {
-					gtksharp_container_base_forall (container, include_internals, cb, data);
-				}
-			} catch (Exception e) {
-				GLib.ExceptionManager.RaiseUnhandledException (e, false);
-			}
-		}
+            internal IntPtr Callback
+            {
+                get
+                {
+                    return cb;
+                }
+            }
+        }
 
-		static void OverrideForallOld (GLib.GType gtype)
-		{
-			if (ForallOldCallback == null)
-				ForallOldCallback = new ForallDelegate (ForallOld_cb);
-			gtksharp_container_override_forall (gtype.Val, ForallOldCallback);
-		}
+        // Compatibility code for old ChildType() virtual method
+        static IntPtr ObsoleteChildType_cb(IntPtr raw)
+        {
+            try
+            {
+                Container obj = GLib.Object.GetObject(raw, false) as Container;
+                GLib.GType gtype = obj.ChildType();
+                return gtype.Val;
+            }
+            catch (Exception e)
+            {
+                GLib.ExceptionManager.RaiseUnhandledException(e, false);
+            }
 
-		[Obsolete ("Override the ForAll(bool,Gtk.Callback) method instead")]
-		[GLib.DefaultSignalHandler (Type=typeof(Gtk.Container), ConnectionMethod="OverrideForallOld")]
-		protected virtual void ForAll (bool include_internals, CallbackInvoker invoker)
-		{
-			gtksharp_container_base_forall (Handle, include_internals, invoker.Callback, invoker.Data);
-		}
+            return GLib.GType.Invalid.Val;
+        }
 
-		static void Forall_cb (IntPtr container, bool include_internals, IntPtr cb, IntPtr data)
-		{
-			try {
-				//GtkContainer's unmanaged dispose calls forall, but by that time the managed object is gone
-				//so it couldn't do anything useful, and resurrecting it would cause a resurrection cycle.
-				//In that case, just chain to the native base in case it can do something.
-				Container obj = (Container) GLib.Object.TryGetObject (container);
-				if (obj != null) {
-					CallbackInvoker invoker = new CallbackInvoker (cb, data);
-					obj.ForAll (include_internals, new Gtk.Callback (invoker.Invoke));
-				} else {
-					gtksharp_container_base_forall (container, include_internals, cb, data);
-				}
-			} catch (Exception e) {
-				GLib.ExceptionManager.RaiseUnhandledException (e, false);
-			}
-		}
+        static ChildTypeNativeDelegate ObsoleteChildTypeVMCallback;
 
-		static void OverrideForall (GLib.GType gtype)
-		{
-			if (ForallCallback == null)
-				ForallCallback = new ForallDelegate (Forall_cb);
-			gtksharp_container_override_forall (gtype.Val, ForallCallback);
-		}
+        static void OverrideObsoleteChildType(GLib.GType gtype)
+        {
+            if (ObsoleteChildTypeVMCallback == null)
+                ObsoleteChildTypeVMCallback = new ChildTypeNativeDelegate(ObsoleteChildType_cb);
+            OverrideChildType(gtype, ObsoleteChildTypeVMCallback); // -> autogenerated method
+        }
 
-		[GLib.DefaultSignalHandler (Type=typeof(Gtk.Container), ConnectionMethod="OverrideForall")]
-		protected virtual void ForAll (bool include_internals, Gtk.Callback callback)
-		{
-			CallbackInvoker invoker;
-			try {
-				invoker = (CallbackInvoker)callback.Target;
-			} catch {
-				throw new ApplicationException ("ForAll can only be called as \"base.ForAll()\". Use Forall() or Foreach().");
-			}
-			gtksharp_container_base_forall (Handle, include_internals, invoker.Callback, invoker.Data);
-		}
+        [Obsolete("Replaced by OnChildType for implementations and SupportedChildType for callers.")]
+        [GLib.DefaultSignalHandler(Type = typeof(Gtk.Container), ConnectionMethod = "OverrideObsoleteChildType")]
+        public virtual GLib.GType ChildType()
+        {
+            return InternalChildType(); // -> autogenerated method
+        }
 
-		// Compatibility code for old ChildType() virtual method
-		static IntPtr ObsoleteChildType_cb (IntPtr raw)
-		{
-			try {
-				Container obj = GLib.Object.GetObject (raw, false) as Container;
-				GLib.GType gtype = obj.ChildType ();
-				return gtype.Val;
-			} catch (Exception e) {
-				GLib.ExceptionManager.RaiseUnhandledException (e, false);
-			}
+        public class ContainerChild
+        {
+            protected Container parent;
+            protected Widget child;
 
-			return GLib.GType.Invalid.Val;
-		}
+            public ContainerChild(Container parent, Widget child)
+            {
+                this.parent = parent;
+                this.child = child;
+            }
 
-		static ChildTypeNativeDelegate ObsoleteChildTypeVMCallback;
+            public Container Parent
+            {
+                get
+                {
+                    return parent;
+                }
+            }
 
-		static void OverrideObsoleteChildType (GLib.GType gtype)
-		{
-			if (ObsoleteChildTypeVMCallback == null)
-				ObsoleteChildTypeVMCallback = new ChildTypeNativeDelegate (ObsoleteChildType_cb);
-			OverrideChildType (gtype, ObsoleteChildTypeVMCallback); // -> autogenerated method
-		}
+            public Widget Child
+            {
+                get
+                {
+                    return child;
+                }
+            }
+        }
 
-		[Obsolete ("Replaced by OnChildType for implementations and SupportedChildType for callers.")]
-		[GLib.DefaultSignalHandler (Type=typeof(Gtk.Container), ConnectionMethod="OverrideObsoleteChildType")]
-		public virtual GLib.GType ChildType() {
-			return InternalChildType (); // -> autogenerated method
-		}
-
-		public class ContainerChild {
-			protected Container parent;
-			protected Widget child;
-
-			public ContainerChild (Container parent, Widget child)
-			{
-				this.parent = parent;
-				this.child = child;
-			}
-
-			public Container Parent {
-				get {
-					return parent;
-				}
-			}
-
-			public Widget Child {
-				get {
-					return child;
-				}
-			}
-		}
-
-		public virtual ContainerChild this [Widget w] {
-			get {
-				return new ContainerChild (this, w);
-			}
-		}
-	}
+        public virtual ContainerChild this[Widget w]
+        {
+            get
+            {
+                return new ContainerChild(this, w);
+            }
+        }
+    }
 }
