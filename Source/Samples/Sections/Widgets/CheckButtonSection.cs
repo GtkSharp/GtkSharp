a13937895f02a667b58c7c647f17539f16a0e0ee
Pedro Larouca <pedro.larouca@msn.com>
2018-01-27 20:38:08 +0000

    CheckButton and ComboBox

---
+++ Source/Samples/Sections/Widgets/CheckButtonSection.cs
@@ -0,0 +1,41 @@
+﻿// This is free and unencumbered software released into the public domain.
+// Happy coding!!! - GtkSharp Team
+
+using Gtk;
+
+namespace Samples
+{
+    [Section(Name = "CheckButton", Category = Category.Widgets)]
+    class CheckButtonSection : ListSection
+    {
+        public CheckButtonSection()
+        {
+            AddItem(CreateCheckButton());
+            AddItem(CreateImageCheckButton());
+        }
+
+        public (string, Widget) CreateCheckButton()
+        {
+            var checkButton = new CheckButton("A simple check button");
+
+            checkButton.Clicked += (sender, e) => 
+                ApplicationOutput.WriteLine(sender, $"Check is now {checkButton.Active}");
+
+            return ("Check button:", checkButton);
+        }
+
+        public (string, Widget) CreateImageCheckButton()
+        {
+            var checkButton = new CheckButton();
+
+            checkButton.Image = Image.NewFromIconName("document-print-symbolic", IconSize.LargeToolbar);
+            checkButton.ImagePosition = PositionType.Right;
+            checkButton.AlwaysShowImage = true;
+
+            checkButton.Clicked += (sender, e) =>
+                ApplicationOutput.WriteLine(sender, $"Check is now {checkButton.Active}");
+
+            return ("Check button with image:", checkButton);
+        }
+    }
+}
---
+++ Source/Samples/Sections/Widgets/ComboBoxSection.cs
@@ -0,0 +1,58 @@
+﻿// This is free and unencumbered software released into the public domain.
+// Happy coding!!! - GtkSharp Team
+
+using Gtk;
+
+namespace Samples
+{
+    [Section(Name = "ComboBox", Category = Category.Widgets)]
+    class ComboBoxSection : ListSection
+    {
+        public ComboBoxSection()
+        {
+            AddItem(CreateSimpleCombo());
+            AddItem(CreateEntryCombo());
+        }
+
+        public (string, Widget) CreateSimpleCombo()
+        {
+            var combo = new ComboBox(new string[] {
+                "First Value",
+                "Second Value",
+                "Third Value"
+            });
+
+            combo.Changed += (sender, e) => 
+                ApplicationOutput.WriteLine(sender, $"Combo selection index is now {combo.Active}");
+
+            return ("ComboBox:", combo);
+        }
+
+        public (string, Widget) CreateEntryCombo()
+        {
+            var combo = ComboBox.NewWithEntry();
+
+            combo.Clear();
+
+            // create values store
+            var store = new ListStore(typeof(string));
+            store.AppendValues("First Value");
+            store.AppendValues("Second Value");
+            store.AppendValues("Third Value");
+
+            // create cell
+            var cell = new CellRendererText();
+
+            // add cell to combo
+            combo.PackStart(cell, true);
+
+            // add text attribute to combo
+            combo.AddAttribute(cell, "text", 0);
+
+            // assign store to combo
+            combo.Model = store;
+
+            return ("ComboBox with Entry:", combo);
+        }
+    }
+}
