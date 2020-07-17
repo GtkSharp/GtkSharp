876066a6cf1e1268bdcc946d73d0015dfc3662f7
lytico <lytio@users.sourceforge.net>
2020-07-17 03:01:22 +0200

    Samples: add Dialog-Sample

--- Source/Samples/Sections/Dialogs/AboutDialogSection.cs
+++ Source/Samples/Sections/Dialogs/AboutDialogSection.cs
@@ -6,12 +6,28 @@ using Gtk;
 
 namespace Samples
 {
-    [Section(ContentType = typeof(AboutDialog), Category = Category.Dialogs)]
-    class AboutDialogSection : Box
-    {
-        public AboutDialogSection() : base(Orientation.Vertical, 0)
-        {
 
-        }
-    }
+	[Section (ContentType = typeof(AboutDialog), Category = Category.Dialogs)]
+	class AboutDialogSection : ListSection
+	{
+		public AboutDialogSection ()
+		{
+			AddItem (CreateAboutDialog ());
+		}
+
+		public (string, Widget) CreateAboutDialog ()
+		{
+			var btn = new Button ("Show");
+			btn.Clicked += (sender, e) => {
+				var dlg = new AboutDialog();
+				dlg.Shown += (s1, e1) => ApplicationOutput.WriteLine (sender, "Shown");
+				var result = dlg.Run ();
+				ApplicationOutput.WriteLine (sender, $"Run result: {result}");
+				dlg.Dispose ();
+			};
+			return ("Dialog:", btn);
+		}
+
+	}
+
 }
